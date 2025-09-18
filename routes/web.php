88fcfb80ea178c1c7b0774c5d4afbcc4ec2e2bb6
agung saputra <agung.dni19@gmail.com>
2025-09-19 00:27:27 +0700

    integrasi template skydash

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Livewire\DashboardPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('dashboard', DashboardPage::class);
